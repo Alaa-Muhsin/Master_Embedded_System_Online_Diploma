a9174a9bda6bffda93df00d616783f36ce32e515
Alaa_Muhsin <alaamuhsin0@gmail.com>
2023-04-22 03:16:09 +0200

    EX1_Lesson3

---
+++ Unit_2/Lesson3_CBasics/EX1/src/EX1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : EX1.c
+ Author      : Alaa Muhsin.
+ Created on  : 18 April 2023.
+ Description : Write C Program to Print a Sentence.
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(void) {
+
+	printf("C Programming"); /* prints C Programming */
+
+	return 0;
+}
